7318ed37dd2580d54c1383d5180ab4cbd27f6694
rezaul <rezaul.karim@selise.ch>
2022-02-15 20:15:32 +0600

    decryption added

--- main/client/wordle-app/src/services/common.service.js
+++ main/client/wordle-app/src/services/common.service.js
@@ -7,6 +7,27 @@ import axios from 'axios';
 
 }
 
+function decrypt(word){
+
+	let s ="";
+
+	for(let i=0;i<word.length;i++){
+		const code = word[i].charCodeAt(0);
+		let newCode = code+i;
+		if(newCode>97 && newCode<=122){
+
+		}
+		else if(newCode>122){
+			newCode = 96+(newCode-122)
+		}
+
+		s+=String.fromCharCode(newCode)
+	
+	}
+	return s;
+
+}
+
 export function decrypt(word){
 
 	let s ="";
